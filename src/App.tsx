0d87658b72d9b289bd39410a69bcb7f6f88ba191
Erik Williamson <erik.williamson@cumplo.com>
2019-12-30 17:23:52 -0300

    add a bit of styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import './App.css';
 
-
 const App: React.FC = () => {
   const [board, setBoard] = useState([
     [0, 0, 0, 0],
@@ -12,24 +11,38 @@ const App: React.FC = () => {
   return (
     <div className="App">
       {board
-        .map((y, iy) => (<div>{y
-          .map((x, ix) =>
-            <span
+        .map((y, iy) => (<div
+          key={`y${iy}`}
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+          }}
+        >{y
+          .map((value, ix) =>
+            <div
+              key={`x${ix}`}
+              style={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                height: 30,
+                width: 30,
+                border: '1px solid black',
+              }}
               onClick={() => setBoard(Object.assign([...board], {
                 [iy]: Object.assign([...board[iy]], {
                   [ix]: Number(!y[ix])
                 })
               }))}
-              style={{ margin: 5 }}
             >
-              {`${x}`}
-            </span>)}
+              {`${value}`}
+            </div>)}
         </div>
         )
         )
       }
 
-    </div>
+    </div >
   );
 }
 
